f4e2f3da4ea880e6a6e65246ebeb0a8f1134e24d
DeanBolte <Dean.bolte7@gmail.com>
2020-08-15 15:17:40 +1000

    changed multiple functions in Euclidean to static

--- Euclidean.cpp
+++ Euclidean.cpp
@@ -2,54 +2,9 @@
 
 #include <iostream>
 
-bool pathfind(Map* map) {
-    // First Node
-    Coords* start = map->locateChar('S');
-    // Final Node
-    Coords* end = map->locateChar('E');
+static Map* map;
 
-    // Recursive find end
-    return path(map, start, end);
-}
-
-float distance(Coords* start, Coords* end) {
-    return sqrt(pow(end->x - start->x, 2) + pow(end->y - start->y, 2));
-}
-
-bool path(Map* map, Coords* start, Coords* end) {
-    // If current node is the final node
-    if(*start == *end) {
-        return true;
-    }
-
-    // identifier for past nodes
-    map->setValue(start, '-');
-
-    // Stack of Nodes
-    std::vector<Coords*> stack;
-
-    // Add adjacent nodes to stack
-    addCoordsToStack(start->x, start->y - 1, map, stack);
-    addCoordsToStack(start->x - 1, start->y, map, stack);
-    addCoordsToStack(start->x, start->y + 1, map, stack);
-    addCoordsToStack(start->x + 1, start->y, map, stack);
-
-    // Sort stack
-    sort(stack, end, 0, stack.size() - 1);
-    
-    // Recursively pathfind from next closest node
-    while(!stack.empty()) {
-        Coords* next = stack.back();
-        if(path(map, next, end)) {
-            map->setValue(start, 'X');
-            return true;
-        }
-        stack.pop_back();
-    }
-    return false;
-}
-
-void addCoordsToStack(int x, int y, Map* map, std::vector<Coords*>& stack) {
+static void addCoordsToStack(int x, int y, std::vector<Coords*>& stack) {
     if(y >= 0 && x >= 0 && (y < map->getHeight()  && x < map->getLength())) {
         if(map->getValue(x, y) == '*' || map->getValue(x, y) == 'E') {
             stack.push_back(new Coords(x, y));
@@ -57,7 +12,7 @@ void addCoordsToStack(int x, int y, Map* map, std::vector<Coords*>& stack) {
     }
 }
 
-void sort(std::vector<Coords*>& stack, Coords* endCoords, int start, int end) {
+static void sort(std::vector<Coords*>& stack, Coords* endCoords, int start, int end) {
     if(start >= end) {
         return;
     }
@@ -82,4 +37,52 @@ void sort(std::vector<Coords*>& stack, Coords* endCoords, int start, int end) {
     
     sort(stack, endCoords, start, pivotIndex - 1);
     sort(stack, endCoords, pivotIndex + 1, end);    
+}
+
+static bool path(Coords* start, Coords* end) {
+    // If current node is the final node
+    if(*start == *end) {
+        return true;
+    }
+
+    // identifier for past nodes
+    map->setValue(start, '-');
+
+    // Stack of Nodes
+    std::vector<Coords*> stack;
+
+    // Add adjacent nodes to stack
+    addCoordsToStack(start->x, start->y - 1, stack);
+    addCoordsToStack(start->x - 1, start->y, stack);
+    addCoordsToStack(start->x, start->y + 1, stack);
+    addCoordsToStack(start->x + 1, start->y, stack);
+
+    // Sort stack
+    sort(stack, end, 0, stack.size() - 1);
+    
+    // Recursively pathfind from next closest node
+    while(!stack.empty()) {
+        Coords* next = stack.back();
+        if(path(next, end)) {
+            map->setValue(start, 'X');
+            return true;
+        }
+        stack.pop_back();
+    }
+    return false;
+}
+
+bool pathfind(Map* mapptr) {
+    map = mapptr;
+    // First Node
+    Coords* start = map->locateChar('S');
+    // Final Node
+    Coords* end = map->locateChar('E');
+
+    // Recursive find end
+    return path(start, end);
+}
+
+float distance(Coords* start, Coords* end) {
+    return sqrt(pow(end->x - start->x, 2) + pow(end->y - start->y, 2));
 }
--- Euclidean.h
+++ Euclidean.h
@@ -10,12 +10,3 @@ bool pathfind(Map* map);
 
 // Calculates the distance from End Node
 float distance(Coords* start, Coords* end);
-
-// Recursive function for finding path to end
-bool path(Map* map, Coords* start, Coords* end);
-
-// Adds the Coords to the stack
-void addCoordsToStack(int x, int y, Map* map, std::vector<Coords*>& stack);
-
-// QuickSort stack by distance to the end
-void sort(std::vector<Coords*>& stack, Coords* endCoords, int start, int end);
